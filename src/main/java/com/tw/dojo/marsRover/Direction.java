d4ab80c66605365a36a037e328e3ba12c824a763
Rachana Devi <rachana.devi@thoughtworks.com>
2021-08-15 12:32:12 +0530

    Tuhin/Rachana | Refactor. Direction gives updatedCoordinate to be added

--- src/main/java/com/tw/dojo/marsRover/Direction.java
+++ src/main/java/com/tw/dojo/marsRover/Direction.java
@@ -4,7 +4,16 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum Direction {
-    N, S, E, W;
+    N(new Coordinate(0, 1)),
+    S(new Coordinate(0, -1)),
+    E(new Coordinate(1, 0)),
+    W(new Coordinate(-1, 0));
+
+    private final Coordinate updatedCoordinate;
+
+    Direction(Coordinate updatedCoordinate) {
+        this.updatedCoordinate = updatedCoordinate;
+    }
 
     Direction turnRight() {
         List<Direction> all = Arrays.asList(N, E, S, W);
@@ -14,5 +23,9 @@ public enum Direction {
     Direction turnLeft() {
         List<Direction> all = Arrays.asList(N, E, S, W);
         return all.get((all.indexOf(this) + 3) % all.size());
+    }
+
+    public Coordinate updatedCoordinate() {
+        return updatedCoordinate;
     }
 }
--- src/main/java/com/tw/dojo/marsRover/MarsRover.java
+++ src/main/java/com/tw/dojo/marsRover/MarsRover.java
@@ -75,19 +75,4 @@ public class MarsRover {
 
         return new Coordinate(xWidth, yWidth);
     }
-
-    private static Coordinate move(Coordinate coordinate, Direction direction) {
-        if (direction.equals(Direction.N)) {
-            coordinate.y += +1;
-        } else if (direction.equals(Direction.S)) {
-            coordinate.y += -1;
-        } else if (direction.equals(Direction.E)) {
-            coordinate.x += +1;
-        } else if (direction.equals(Direction.W)) {
-            coordinate.x += -1;
-        }
-
-        return coordinate;
-    }
-
 }
